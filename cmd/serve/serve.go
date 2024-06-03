c09de838b792414e1e781b658bffc5ee100d2453
George Duffett <george.duffett@icloud.com>
2024-06-03 19:40:02 +0100

    feat: write server response

--- cmd/serve/serve.go
+++ cmd/serve/serve.go
@@ -1,20 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log/slog"
 	"net/http"
 
 	"github.com/g-duff/groceries/pkg/shoppinglist"
 )
 
-
 func main() {
 
 	slog.Info("Starting...")
 
-
 	http.HandleFunc("/shoppinglist", handle)
-
 
 	http.ListenAndServe(":8080", nil)
 }
@@ -24,13 +23,17 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	meals := [10]string{"chips", "bar"}
 
 	dd := shoppinglist.MealsDataSpy{}
-	// dd := shoppinglist.MealsDataFiles{ 
+	// dd := shoppinglist.MealsDataFiles{
 	// 	Directory: "./testdata",
 	// }
 
 	groceriesListItems := shoppinglist.Create(&dd, meals[:])
 
-	for _, g := range(groceriesListItems) {
-		slog.Info(g.ToString())
+	resp, err := json.Marshal(groceriesListItems)
+
+	if err != nil {
+		io.WriteString(w, "{ \"message\": \"request failed\" }")
 	}
+
+	io.WriteString(w, string(resp))
 }
